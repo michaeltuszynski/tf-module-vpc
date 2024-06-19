0e33c5a491aa13279b0f03c8d7f5742b5c374f1a
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-19 12:13:09 -0700

    vpc module v1.0.2

--- outputs.tf
+++ outputs.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
   value       = aws_vpc.this_vpc.id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
   description = "The ID of the public subnet"
-  value       = length(aws_subnet.public_subnet) > 0 ? aws_subnet.public_subnet[0].id : null
+  value       = var.subnet_type == "public" || var.subnet_type == "both" ? aws_subnet.public_subnet[*].id : []
 }
 
-output "private_subnet_id" {
-  description = "The ID of the private subnet"
-  value       = length(aws_subnet.private_subnet) > 0 ? aws_subnet.private_subnet[0].id : null
+output "private_subnet_ids" {
+  description = "The IDs of the private subnets"
+  value       = var.subnet_type == "private" || var.subnet_type == "both" ? aws_subnet.private_subnet[*].id : []
 }
 
 output "nat_gateway_id" {
-  description = "The ID of the NAT gateway"
-  value       = length(aws_nat_gateway.this_nat_gateway) > 0 ? aws_nat_gateway.this_nat_gateway[0].id : null
+  description = "The IDs of the private subnets"
+  value       = var.subnet_type == "private" || var.subnet_type == "both" ? aws_subnet.this_nat_gateway[0].id : []
 }
