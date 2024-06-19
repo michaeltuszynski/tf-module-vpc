ebbc0026e1a1e91c6668ce43ba53c876a1c0ebb5
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-19 12:16:20 -0700

    fix nat gateway ref

--- outputs.tf
+++ outputs.tf
@@ -15,5 +15,5 @@ output "private_subnet_ids" {
 
 output "nat_gateway_id" {
   description = "The IDs of the private subnets"
-  value       = var.subnet_type == "private" || var.subnet_type == "both" ? aws_subnet.this_nat_gateway[0].id : []
+  value       = var.subnet_type == "private" || var.subnet_type == "both" ? aws_subnet.this_nat_gateway.id : []
 }
