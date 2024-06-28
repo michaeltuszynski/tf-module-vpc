de35286dcf7ef57530dd20610308b04c7078e90d
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-28 08:02:54 -0700

    add providers

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+  alias = "default"
+}
+
 # Data source to get available availability zones
 data "aws_availability_zones" "available" {}
 
 resource "aws_vpc" "this_vpc" {
+  provider             = aws.default
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
@@ -14,6 +19,7 @@ resource "aws_vpc" "this_vpc" {
 
 # Conditionally create public subnets
 resource "aws_subnet" "public_subnet" {
+  provider                = aws.default
   count                   = length(var.public_subnet_cidrs) > 0 ? length(var.public_subnet_cidrs) : 0
   vpc_id                  = aws_vpc.this_vpc.id
   cidr_block              = var.public_subnet_cidrs[count.index]
@@ -28,6 +34,7 @@ resource "aws_subnet" "public_subnet" {
 
 # Conditionally create private subnets
 resource "aws_subnet" "private_subnet" {
+  provider          = aws.default
   count             = length(var.private_subnet_cidrs) > 0 ? length(var.private_subnet_cidrs) : 0
   vpc_id            = aws_vpc.this_vpc.id
   cidr_block        = var.private_subnet_cidrs[count.index]
@@ -39,8 +46,9 @@ resource "aws_subnet" "private_subnet" {
 }
 
 resource "aws_internet_gateway" "this_vpc_igw" {
-  count  = length(var.public_subnet_cidrs) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this_vpc.id
+  provider = aws.default
+  count    = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+  vpc_id   = aws_vpc.this_vpc.id
 
   tags = {
     Type = "this_igw"
@@ -49,6 +57,7 @@ resource "aws_internet_gateway" "this_vpc_igw" {
 }
 
 resource "aws_nat_gateway" "this_nat_gateway" {
+  provider      = aws.default
   count         = length(var.private_subnet_cidrs) > 0 ? 1 : 0
   allocation_id = aws_eip.this_eip[0].id
   subnet_id     = element(aws_subnet.public_subnet.*.id, 0)
@@ -60,8 +69,9 @@ resource "aws_nat_gateway" "this_nat_gateway" {
 }
 
 resource "aws_eip" "this_eip" {
-  count  = length(var.private_subnet_cidrs) > 0 ? 1 : 0
-  domain = "vpc"
+  provider = aws.default
+  count    = length(var.private_subnet_cidrs) > 0 ? 1 : 0
+  domain   = "vpc"
 
   tags = {
     Type = "this_eip"
@@ -70,8 +80,9 @@ resource "aws_eip" "this_eip" {
 }
 
 resource "aws_route_table" "private_route_table" {
-  count  = length(var.private_subnet_cidrs) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this_vpc.id
+  provider = aws.default
+  count    = length(var.private_subnet_cidrs) > 0 ? 1 : 0
+  vpc_id   = aws_vpc.this_vpc.id
 
   route {
     cidr_block     = "0.0.0.0/0"
@@ -85,14 +96,16 @@ resource "aws_route_table" "private_route_table" {
 }
 
 resource "aws_route_table_association" "private_route_table_association" {
+  provider       = aws.default
   count          = length(var.private_subnet_cidrs) > 0 ? length(var.private_subnet_cidrs) : 0
   subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_route_table[0].id
 }
 
 resource "aws_route_table" "public_route_table" {
-  count  = length(var.public_subnet_cidrs) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this_vpc.id
+  provider = aws.default
+  count    = length(var.public_subnet_cidrs) > 0 ? 1 : 0
+  vpc_id   = aws_vpc.this_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -106,6 +119,7 @@ resource "aws_route_table" "public_route_table" {
 }
 
 resource "aws_route_table_association" "public_route_table_association" {
+  provider       = aws.default
   count          = length(var.public_subnet_cidrs) > 0 ? length(var.public_subnet_cidrs) : 0
   subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = element(aws_route_table.public_route_table.*.id, count.index)
